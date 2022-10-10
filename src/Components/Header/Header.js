83a5f5d60fa304fa3cc2190176e3037a482fec27
unknown <redoyk2015@gmail.com>
2022-10-11 00:34:36 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import image from '../../images/head.jpg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col-lg-6 headTitle">
+                    <h1>PRACTICE MORE AND</h1>
+                    <h1>BECOME <span style={{ color: '#465DFF' }}>PRO...</span></h1>
+                    <p className='text-muted my-3'>Quizi is an online based quiz website where you can give an exam on your favourite subject. it will provide you multiple choice question according to your favourite subjects..</p>
+                    <a href='#' className='btn btHead'>Let's Start</a>
+                </div>
+
+                <div className="col-lg-6">
+                    <img className='img-fluid imgHead' src={image} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
-
+            <section className='section1'>
+                <Header></Header>
+            </section>
         </div>
     );
 };
